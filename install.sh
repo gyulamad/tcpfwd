d2316bdc8906c6e9255b62cb46b8ea691e8e7ad6
Gyula Madarasz <gyula.madarasz@pm.me>
2026-04-19 14:03:43 +0100

    update for pi

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt install ccache
+sudo apt install wakeonlan
 git clone https://github.com/gyulamad/autobuild
 git clone https://github.com/gyulamad/cpptools-misc cpptools/misc
 git clone https://github.com/gyulamad/cpptools-net cpptools/net
