7225b1f037ecccfaea17b253c77db3fd41742641
Gyula Madarasz <gyula.madarasz@pm.me>
2026-06-25 11:46:58 +0100

    zzz examples

--- zzz-proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Get the directory where THIS script is located (handles symlinks)
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-cd "$SCRIPT_DIR" && ./builder $SCRIPT_DIR/cpptools/net/examples/zzz-proxy/zzz-proxy.cpp --mode=strict --run --run-args="9090 monster:8080 --startcmd=$SCRIPT_DIR/cpptools/net/examples/zzz-proxy/srv/start.sh >> $SCRIPT_DIR/cpptools/net/examples/zzz-proxy/srv/output.log 2>&1 --stopcmd=$SCRIPT_DIR/cpptools/net/examples/zzz-proxy/srv/stop.sh >> $SCRIPT_DIR/cpptools/net/examples/zzz-proxy/srv/output.log 2>&1 --timeout=180"
